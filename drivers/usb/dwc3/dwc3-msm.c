5091a8e822d5fc9072e01255c4ba32074cd605e8
George Hotz <geohot@gmail.com>
2016-11-20 09:38:47 -0800

    otg switch on by default

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -244,7 +244,7 @@ struct dwc3_msm {
 	unsigned int		lpm_to_suspend_delay;
 	bool			init;
 };
-int otg_switch;
+int otg_switch = 1;  // comma: OTG is always on
 struct dwc3_msm *opmdwc;
 bool gadget_start = false;/*Anderson-Avoid_warning_about_irq+ */
 
